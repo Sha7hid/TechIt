7115400b041a8ed9736c7f2170d6c34472ba93ee
sha7hid <shahidshafadshahad@gmail.com>
2023-08-09 12:00:08 +0530

    navbar completed

--- techit/src/app/page.js
+++ techit/src/app/page.js
@@ -1,9 +1,40 @@
-import Image from 'next/image'
-
+"use client";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { luck } from "@/fonts";
+import styles from "./home.module.css";
+import {TiShoppingCart} from 'react-icons/ti';
+import {IoPersonSharp} from 'react-icons/io5'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     Hello World
-    </main>
-  )
+    <Navbar collapseOnSelect expand="lg" className={styles.customColor}>
+      <Container className={luck.className}>
+        <Navbar.Brand href="#home" className={styles.customHead}>
+          TechIt
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav className="me-auto">
+            <Nav.Link className={styles.custom} href="#features">
+              Home
+            </Nav.Link>
+            <Nav.Link className={styles.custom} href="#pricing">
+              Products
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link className={styles.custom} href="#deets">
+              <span><TiShoppingCart size={50}/></span>
+            </Nav.Link>
+            <Nav.Link className={styles.custom} href="#memes">
+            <span><IoPersonSharp size={45}/></span>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
---
+++ techit/src/fonts.js
@@ -0,0 +1,5 @@
+import { Luckiest_Guy } from "@next/font/google"
+export const luck = Luckiest_Guy({
+    variable: '--title-font' ,
+    subsets: ['latin'],
+    weight: '400'})
