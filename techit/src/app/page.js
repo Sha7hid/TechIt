39a6f6d66fbb2e29350d5c05347ae99cc868f059
sha7hid <shahidshafadshahad@gmail.com>
2023-08-09 20:17:02 +0530

    homepage responsiveness completed

--- techit/src/app/page.js
+++ techit/src/app/page.js
@@ -3,38 +3,61 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { luck } from "@/fonts";
+import { luck,spartan } from "@/fonts";
 import styles from "./home.module.css";
-import {TiShoppingCart} from 'react-icons/ti';
-import {IoPersonSharp} from 'react-icons/io5'
+import { TiShoppingCart } from "react-icons/ti";
+import { IoPersonSharp } from "react-icons/io5";
+import { Row ,Col} from "react-bootstrap";
+import Image from "next/image";
+import title from '../../public/title.jpg'
 export default function Home() {
   return (
-    <Navbar collapseOnSelect expand="lg" className={styles.customColor}>
-      <Container className={luck.className}>
-        <Navbar.Brand href="#home" className={styles.customHead}>
-          TechIt
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto"></Nav>
-          <Nav className="me-auto">
-            <Nav.Link className={styles.custom} href="#features">
-              Home
-            </Nav.Link>
-            <Nav.Link className={styles.custom} href="#pricing">
-              Products
-            </Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link className={styles.custom} href="#deets">
-              <span><TiShoppingCart size={50}/></span>
-            </Nav.Link>
-            <Nav.Link className={styles.custom} href="#memes">
-            <span><IoPersonSharp size={45}/></span>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar collapseOnSelect expand="lg" className={styles.customColor}>
+        <Container className={luck.className}>
+          <Navbar.Brand href="#home" className={styles.customHead}>
+            TechIt
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav className="me-auto">
+              <Nav.Link className={styles.custom} href="#features">
+                Home
+              </Nav.Link>
+              <Nav.Link className={styles.custom} href="#pricing">
+                Products
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link className={styles.custom} href="#deets">
+                <span>
+                  <TiShoppingCart size={40} />
+                </span>
+              </Nav.Link>
+              <Nav.Link className={styles.custom} href="#memes">
+                <span>
+                  <IoPersonSharp size={35} />
+                </span>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <main className={styles.container}>
+        <Row>
+          <Col lg={6}>
+          <div className={spartan.className}>
+        <h1 className={styles.title}>Buy a laptop of your choice</h1>
+        <p className={styles.para}>You want it we have it</p>
+        </div>
+          </Col>
+          <Col lg={6} className={styles.image}>
+            <Image src={title} height={600} width={450}/>
+          </Col>
+        </Row>
+
+      </main>
+    </>
   );
 }
--- techit/src/fonts.js
+++ techit/src/fonts.js
@@ -1,5 +1,8 @@
-import { Luckiest_Guy } from "@next/font/google"
+import { Luckiest_Guy,League_Spartan } from "@next/font/google"
 export const luck = Luckiest_Guy({
-    variable: '--title-font' ,
     subsets: ['latin'],
     weight: '400'})
+    export const spartan = League_Spartan({
+        subsets: ['latin'],
+        weight: '600'
+    })
