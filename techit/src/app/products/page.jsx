2ad788a11338b8ca90bd5f22e74d3ccfd7856b2b
sha7hid <shahidshafadshahad@gmail.com>
2023-08-10 17:58:42 +0530

    responsive home and products 1

--- techit/src/app/products/page.jsx
+++ techit/src/app/products/page.jsx
@@ -2,58 +2,58 @@ export default function Products() {
     return (
         <main class="min-h-screen bg-test-color flex items-start justify-start">
         
- <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 ml-11 mt-11">
+ <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 ml-11 mr-10 mb-11 mt-11">
     
  <div class="w-full lg:w-96 bg-test-color2 rounded-lg shadow-lg flex text-white">
-  <img src="https://sm.ign.com/ign_in/tech/h/hp-victus-/hp-victus-15_j8t1.png" alt="Laptop Photo" class="w-52 h-auto rounded-lg"/>
-  <div class="pl-9 pt-4 pb-4 flex flex-col justify-between">
+  <img src="https://sm.ign.com/ign_in/tech/h/hp-victus-/hp-victus-15_j8t1.png" alt="Laptop Photo" class="w-52  h-auto rounded-lg"/>
+  <div class="lg:pl-9 lg:pt-4 pb-4 md:pt-4 md:pl-9 sm:pl-5 sm:pt-2 flex flex-col justify-between">
     <div>
-      <h3 class="text-xl font-semibold mb-2 pr-6">HP VICTUS</h3>
-      <p class="text-white-600 pl-2 pr-3">$1,299</p>
+      <h3 class="sm:text-xs md:text-base lg:text-xl font-semibold mb-2 lg:pr-6 md:pr-4 sm:pr-2 ">HP VICTUS</h3>
+      <p class="text-white-600 lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2 sm:text-xs md:text-base lg:text-base">$1,299</p>
     </div>
-    <div class="mt-2">
-      <ul class="list-disc pl-2 pr-3">
+    <div class="mt-2 sm:text-xs md:text-base lg:text-base">
+      <ul class="list-disc lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2">
       <li>i7</li>
         <li>526 SSD</li>
       </ul>
     </div>
-    <button class="mt-4 mr-3 bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 transition duration-300">Buy Now</button>
+    <button class="sm:text-xs md:text-base lg:text-base lg:mt-4 lg:mr-3 md:mt-4 md:mr-3 sm:mt-2 sm:mr-1  bg-blue-500 text-white rounded-md lg:px-4 lg:py-2 md:px-4 md:py-2 sm:px-2 sm:py-2 hover:bg-blue-600 transition duration-300">Buy Now</button>
   </div>
 </div>
 
 <div class="w-full lg:w-96 bg-test-color2 rounded-lg shadow-lg flex text-white">
-  <img src="https://sm.ign.com/ign_in/tech/h/hp-victus-/hp-victus-15_j8t1.png" alt="Laptop Photo" class="w-52 h-auto rounded-lg"/>
-  <div class="pl-9 pt-4 pb-4 flex flex-col justify-between">
+  <img src="https://sm.ign.com/ign_in/tech/h/hp-victus-/hp-victus-15_j8t1.png" alt="Laptop Photo" class="w-52  h-auto rounded-lg"/>
+  <div class="lg:pl-9 lg:pt-4 pb-4 md:pt-4 md:pl-9 sm:pl-5 sm:pt-2 flex flex-col justify-between">
     <div>
-      <h3 class="text-xl font-semibold mb-2 pr-6">HP VICTUS</h3>
-      <p class="text-white-600 pl-2 pr-3">$1,299</p>
+      <h3 class="sm:text-xs md:text-base lg:text-xl font-semibold mb-2 lg:pr-6 md:pr-4 sm:pr-2 ">HP VICTUS</h3>
+      <p class="text-white-600 lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2 sm:text-xs md:text-base lg:text-base">$1,299</p>
     </div>
-    <div class="mt-2">
-      <ul class="list-disc pl-2 pr-3">
+    <div class="mt-2 sm:text-xs md:text-base lg:text-base">
+      <ul class="list-disc lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2">
       <li>i7</li>
         <li>526 SSD</li>
       </ul>
     </div>
-    <button class="mt-4 mr-3 bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 transition duration-300">Buy Now</button>
+    <button class="sm:text-xs md:text-base lg:text-base lg:mt-4 lg:mr-3 md:mt-4 md:mr-3 sm:mt-2 sm:mr-1  bg-blue-500 text-white rounded-md lg:px-4 lg:py-2 md:px-4 md:py-2 sm:px-2 sm:py-2 hover:bg-blue-600 transition duration-300">Buy Now</button>
   </div>
 </div>
 
 
 
 <div class="w-full lg:w-96 bg-test-color2 rounded-lg shadow-lg flex text-white">
-  <img src="https://sm.ign.com/ign_in/tech/h/hp-victus-/hp-victus-15_j8t1.png" alt="Laptop Photo" class="w-52 h-auto rounded-lg"/>
-  <div class="pl-9 pt-4 pb-4 flex flex-col justify-between">
+  <img src="https://sm.ign.com/ign_in/tech/h/hp-victus-/hp-victus-15_j8t1.png" alt="Laptop Photo" class="w-52  h-auto rounded-lg"/>
+  <div class="lg:pl-9 lg:pt-4 pb-4 md:pt-4 md:pl-9 sm:pl-5 sm:pt-2 flex flex-col justify-between">
     <div>
-      <h3 class="text-xl font-semibold mb-2 pr-6">HP VICTUS</h3>
-      <p class="text-white-600 pl-2 pr-3">$1,299</p>
+      <h3 class="sm:text-xs md:text-base lg:text-xl font-semibold mb-2 lg:pr-6 md:pr-4 sm:pr-2 ">HP VICTUS</h3>
+      <p class="text-white-600 lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2 sm:text-xs md:text-base lg:text-base">$1,299</p>
     </div>
-    <div class="mt-2">
-      <ul class="list-disc pl-2 pr-3">
+    <div class="mt-2 sm:text-xs md:text-base lg:text-base">
+      <ul class="list-disc lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2">
       <li>i7</li>
         <li>526 SSD</li>
       </ul>
     </div>
-    <button class="mt-4 mr-3 bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 transition duration-300">Buy Now</button>
+    <button class="sm:text-xs md:text-base lg:text-base lg:mt-4 lg:mr-3 md:mt-4 md:mr-3 sm:mt-2 sm:mr-1  bg-blue-500 text-white rounded-md lg:px-4 lg:py-2 md:px-4 md:py-2 sm:px-2 sm:py-2 hover:bg-blue-600 transition duration-300">Buy Now</button>
   </div>
 </div>
 
