441e0d2f9361aad93f47417861e05c0eb6081638
sha7hid <shahidshafadshahad@gmail.com>
2023-08-13 22:59:22 +0530

    edit options.ts

--- techit/src/app/api/auth/[...nextauth]/options.ts
+++ techit/src/app/api/auth/[...nextauth]/options.ts
@@ -22,6 +22,7 @@ if(credentials?.email === user.email && credentials?.password === user.password)
 }
         })
     ],
+    secret:'VBh/SS2fQ34dX3Ii08j14erMDC2k5SaNX5bCbI+msJU=',
     pages:{
         signIn:"/auth/signIn"
 
