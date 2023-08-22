b266a2fb89179655fbe03e02411b17aa365d8ed9
sha7hid <shahidshafadshahad@gmail.com>
2023-08-22 23:24:43 +0530

    updt 2

--- techit/src/app/cart/page.jsx
+++ techit/src/app/cart/page.jsx
@@ -5,7 +5,6 @@ import { useRouter } from "next/navigation"; // Import useRouter
 import { useState, useEffect } from "react";
 import pro from "../../../public/profile.png";
 import Link from "next/link";
-
 export default function Cart() {
   const { data: session, status } = useSession();
   const router = useRouter();
@@ -54,13 +53,11 @@ export default function Cart() {
         .catch((error) => console.error("Error fetching user data:", error));
     }
   }, [status, session]);
-
   useEffect(() => {
     if (userData) {
       fetchCartData(userData.user_id);
     }
   }, [userData]);
-
   useEffect(() => {
     if (cartData) {
       cartData.map((result) => {
@@ -68,7 +65,6 @@ export default function Cart() {
       });
     }
   }, [cartData]);
-
   if (status === "loading") {
     return (
       <div className="flex flex-col justify-center items-center h-screen bg-test-color">
@@ -76,7 +72,6 @@ export default function Cart() {
       </div>
     );
   }
-
   if (!session) {
     router.push("/auth/signIn");
     return null;
@@ -84,9 +79,10 @@ export default function Cart() {
 
   return (
     <div className="flex flex-col justify-start items-center h-screen bg-test-color">
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-12 ml-11 mr-10 mb-11 mt-11">
-      {isLoadingProductData ? (
-        <div className="min-[320px]:w-60 sm:w-60 md:w-80 lg:w-96 min-[1025px]:w-96 bg-test-color2 rounded-lg shadow-lg flex text-white mb-4">
+      <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 ml-11 mr-10 mb-11 mt-11">
+        {isLoadingProductData
+          ? (
+            <div className="w-full lg:w-96 bg-test-color2 rounded-lg shadow-lg flex text-white">
               <div className="w-52 h-auto rounded-lg bg-gray-500 animate-pulse"></div>
               <div className="min-[320px]:pl-2 min-[320px]:pr-2 min-[320px]:pt-2 lg:pl-9 lg:pt-4 pb-4 md:pt-4 md:pl-9 sm:pl-5 sm:pt-2 flex flex-col justify-between">
                 <div>
@@ -95,7 +91,7 @@ export default function Cart() {
                 </div>
                 <div className="mt-2 sm:text-xs md:text-base lg:text-base  min-[320px]:text-xs">
                   <div className="bg-gray-500 h-7 w-32 animate-pulse"></div>
-                </div>
+   </div>
                 <div className="bg-gray-500 h-6 w-20 mt-2 animate-pulse"></div>
               </div>
             </div>
@@ -103,41 +99,38 @@ export default function Cart() {
           : productData &&
             productData.map((result) => (
               <>
-                <div className=" h-auto min-[320px]:w-60 sm:w-64 md:w-80 lg:w-96 min-[1025px]:w-96 bg-test-color2 rounded-lg shadow-lg flex  text-white">
+                <div class="w-full lg:w-96 bg-test-color2 rounded-lg shadow-lg flex text-white">
                   <img
                     src={result.Image}
                     alt="Laptop Photo"
-                    className="min-[320px]:w-32  sm:w-52 md:w-60 lg:w-52 min-[1025px]:w-52 h-auto rounded-lg"
+                    class="w-52  h-auto rounded-lg"
                   />
-                 <div className=" min-[320px]:pl-2 min-[320px]:pt-1 lg:pl-9 lg:pt-4 lg:pb-4 md:pt-4 md:pl-9 sm:pl-5 sm:pt-2 flex flex-col justify-center items-center">
-            <div>
-              <h3 className="min-[320px]:text-xs sm:text-xs md:text-base lg:text-xl font-semibold lg:mb-2 md:mb-2 sm:mb-1 lg:pr-3 md:pr-2 sm:pr-1 md:line-clamp-1 sm:line-clamp-2 min-[320px]:line-clamp-1  ">
-                {result.Product_Name}
-              </h3>
-              <p className="min-[320px]:text-xs text-white-600 lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2 sm:text-xs md:text-base lg:text-base ">
-                {result.Price}
-              </p>
-            </div>
-            <div className="lg:mt-2 md:mt-2 sm:mt-1 sm:text-xs md:text-base lg:text-base ">
-              <button
-                className="min-[320px]:text-xs sm:text-xs md:text-base lg:text-base  lg:mt-4 lg:mr-3 md:mt-4 md:mr-3 sm:mt-2 sm:mr-1 min-[320px]:mb-1  bg-red-600 text-white rounded-md  lg:px-2 lg:py-1 md:px-2 md:py-2 sm:px-1 sm:py-0.5 min-[320px]:px-1 min-[320px]:py-0.5"
-                onClick={() => handleRemoveItem(result.cart_id)} // Replace with your remove item function
-              >
-                Remove
-              </button>
-            </div>
-            <Link
-              className="min-[320px]:text-xs sm:text-xs md:text-base lg:text-base   lg:mt-4 lg:mr-3 md:mt-4 md:mr-3 sm:mt-2 sm:mr-1 bg-black text-white rounded-md  lg:px-4 lg:py-2 md:px-1 md:py-2 sm:px-2 sm:py-2 min-[320px]:px-0.5 min-[320px]:py-1 hover:bg-blue-600 transition duration-300"
-              href={`/cart/${result.cart_id}`}
-            >
-              Place order
-            </Link>
-          </div>
-        </div>
+                  <div class=" min-[320px]:pl-2 min-[320px]:pr-2 min-[320px]:pt-2 lg:pl-9 lg:pt-4 pb-4 md:pt-4 md:pl-9 sm:pl-5 sm:pt-2 flex flex-col justify-between">
+                    <div>
+                      <h3 class="sm:text-xs md:text-base lg:text-xl font-semibold mb-2 lg:pr-6 md:pr-4 sm:pr-2   min-[320px]:text-sm">
+                        {result.Product_Name}
+                      </h3>
+                      <p class="text-white-600 lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2 sm:text-xs md:text-base lg:text-base  min-[320px]:text-sm">
+                        {result.Price}
+                      </p>
+                    </div>
+                    <div class="mt-2 sm:text-xs md:text-base lg:text-base  min-[320px]:text-xs">
+                      <ul class="list-disc lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2">
+                        <li>{result.Processor}</li>
+                        <li>{result.Memory}</li>
+                      </ul>
+                    </div>
+                    <Link
+                      className="min-[320px]:text-xs sm:text-xs md:text-base lg:text-base min-[320px]:mt-2 min-[320px]:mr-2 lg:mt-4 lg:mr-3 md:mt-4 md:mr-3 sm:mt-2 sm:mr-1 bg-black text-white rounded-md min-[320px]:px-2 min-[320px]:py-1 lg:px-4 lg:py-2 md:px-4 md:py-2 sm:px-2 sm:py-2 hover:bg-blue-600 transition duration-300"
+                      href={`/cart/${result.cart_id}`}
+                    >
+                      Place order
+                    </Link>
+                  </div>
+                </div>
               </>
             ))}
       </div>
     </div>
   );
 }
-
