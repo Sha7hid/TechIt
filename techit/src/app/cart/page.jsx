bb6372da05e2eb6eaff189750ccdd26a694bf4ad
sha7hid <shahidshafadshahad@gmail.com>
2023-08-25 14:09:09 +0530

    remove button added cart its working and only button added in orders.

--- techit/src/app/cart/page.jsx
+++ techit/src/app/cart/page.jsx
@@ -3,7 +3,6 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation"; // Import useRouter
 import { useState, useEffect } from "react";
-import pro from "../../../public/profile.png";
 import Link from "next/link";
 export default function Cart() {
   const { data: session, status } = useSession();
@@ -12,6 +11,8 @@ export default function Cart() {
   const [cartData, setCartData] = useState(null);
   const [productData, setProductData] = useState([]);
   const [isLoadingProductData, setIsLoadingProductData] = useState(true);
+  const [isAddingToCart, setIsAddingToCart] = useState(false); 
+  const [success,setSuccess] = useState(false)
 //test
   const fetchCartData = async (userId) => {
     try {
@@ -72,14 +73,47 @@ export default function Cart() {
       </div>
     );
   }
+  const handleRemoveCartItem = async (params) => {
+    if (!session) {
+      // User is not logged in, handle this scenario (e.g., redirect to login).
+      return;
+    }
+    const cart_id = params
+  console.log(cart_id)
+    try {
+      setIsAddingToCart(true);
+      // Send a POST request to add the order.
+      const response = await fetch(`/api/cartdelete/${cart_id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      const responseData = await response.json();
+
+      if (responseData) {
+        setSuccess(true)
+        // Handle success (e.g., show a success message).
+        console.log('item removed successfully');
+      } else {
+        // Handle the error case.
+        console.error('Failed to remove:', responseData.error);
+      }
+    } catch (error) {
+      console.error('Error removing item:', error);
+    } finally{
+      setIsAddingToCart(false);
+    }
+  };
   if (!session) {
     router.push("/auth/signIn");
     return null;
   }
 
   return (
-    <div className="flex flex-col justify-start items-center h-screen bg-test-color">
-      <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 ml-11 mr-10 mb-11 mt-11">
+    <div class="min-h-screen bg-test-color flex items-start justify-start">
+     <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 ml-11 mr-10 mb-11 mt-11">
         {isLoadingProductData
           ? (
             <div className="w-full lg:w-96 bg-test-color2 rounded-lg shadow-lg flex text-white">
@@ -99,36 +133,21 @@ export default function Cart() {
           : productData &&
             productData.map((result) => (
               <>
-                <div class="w-full lg:w-96 bg-test-color2 rounded-lg shadow-lg flex text-white">
-                  <img
-                    src={result.Image}
-                    alt="Laptop Photo"
-                    class="w-52  h-auto rounded-lg"
-                  />
-                  <div class=" min-[320px]:pl-2 min-[320px]:pr-2 min-[320px]:pt-2 lg:pl-9 lg:pt-4 pb-4 md:pt-4 md:pl-9 sm:pl-5 sm:pt-2 flex flex-col justify-between">
-                    <div>
-                      <h3 class="sm:text-xs md:text-base lg:text-xl font-semibold mb-2 lg:pr-6 md:pr-4 sm:pr-2   min-[320px]:text-sm">
-                        {result.Product_Name}
-                      </h3>
-                      <p class="text-white-600 lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2 sm:text-xs md:text-base lg:text-base  min-[320px]:text-sm">
-                        {result.Price}
-                      </p>
-                    </div>
-                    <div class="mt-2 sm:text-xs md:text-base lg:text-base  min-[320px]:text-xs">
-                      <ul class="list-disc lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2">
-                        <li>{result.Processor}</li>
-                        <li>{result.Memory}</li>
-                      </ul>
-                    </div>
-                    <Link
-                      className="min-[320px]:text-xs sm:text-xs md:text-base lg:text-base min-[320px]:mt-2 min-[320px]:mr-2 lg:mt-4 lg:mr-3 md:mt-4 md:mr-3 sm:mt-2 sm:mr-1 bg-black text-white rounded-md min-[320px]:px-2 min-[320px]:py-1 lg:px-4 lg:py-2 md:px-4 md:py-2 sm:px-2 sm:py-2 hover:bg-blue-600 transition duration-300"
-                      href={`/cart/${result.cart_id}`}
-                    >
-                      Place order
-                    </Link>
-                  </div>
-                </div>
-              </>
+               <div class="w-full lg:w-96 bg-test-color2 rounded-lg shadow-lg flex text-white">
+  <img src={result.Image} alt="Laptop Photo" class="w-52  h-auto rounded-lg"/>
+  <div class=" min-[320px]:pl-2 min-[320px]:pr-2 min-[320px]:pt-2 lg:pl-9 lg:pt-4 pb-4 md:pt-4 md:pl-9 sm:pl-5 sm:pt-2 flex flex-col justify-between">
+    <div>
+      <h3 class="sm:text-xs md:text-base lg:text-xl font-semibold mb-2 lg:pr-6 md:pr-4 sm:pr-2   min-[320px]:text-sm">{result.Product_Name}</h3>
+      <p class="text-white-600 lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2 sm:text-xs md:text-base lg:text-base  min-[320px]:text-sm">{result.Price}</p>
+    </div>
+    <Link class="min-[320px]:text-xs sm:text-xs md:text-base lg:text-base min-[320px]:mt-2 min-[320px]:mr-2 lg:mt-4 lg:mr-3 md:mt-4 md:mr-3 sm:mt-2 sm:mr-1  bg-black text-white rounded-md min-[320px]:px-2 min-[320px]:py-1 lg:px-4 lg:py-2 md:px-4 md:py-2 sm:px-2 sm:py-2 hover:bg-blue-600 transition duration-300" href={`/cart/${result.cart_id}`} >Place Order</Link>
+    <button class="min-[320px]:text-xs sm:text-xs md:text-base lg:text-base min-[320px]:mt-2 min-[320px]:mr-2 lg:mt-4 lg:mr-3 md:mt-4 md:mr-3 sm:mt-2 sm:mr-1  bg-red-600 text-white rounded-md min-[320px]:px-2 min-[320px]:py-1 lg:px-4 lg:py-2 md:px-4 md:py-2 sm:px-2 sm:py-2" 
+        onClick={() => handleRemoveCartItem(result.cart_id)}
+       disabled={isAddingToCart}>  {isAddingToCart ? 'Removing from cart' : 'Remove'}</button> 
+    {success ? <p>successfully removed item</p> :<p></p>}
+  </div>
+</div>
+</>
             ))}
       </div>
     </div>
--- techit/src/app/orders/page.jsx
+++ techit/src/app/orders/page.jsx
@@ -83,8 +83,8 @@ export default function Orders() {
   }
 console.log(productData)
   return (
-    <div className="flex flex-col justify-start items-center h-screen bg-test-color">
-      <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8 ml-11 mr-10 mb-11 mt-11">
+    <div class="min-h-screen bg-test-color flex items-start justify-start">
+     <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 ml-11 mr-10 mb-11 mt-11">
         {isLoadingProductData
           ? (
             <div className="w-full lg:w-96 bg-test-color2 rounded-lg shadow-lg flex text-white">
@@ -105,27 +105,13 @@ console.log(productData)
             productData.map((result) => (
               <>
                 <div class="w-full lg:w-96 bg-test-color2 rounded-lg shadow-lg flex text-white">
-                  <img
-                    src={result.Image}
-                    alt="Laptop Photo"
-                    class="w-52  h-auto rounded-lg"
-                  />
-                  <div class=" min-[320px]:pl-2 min-[320px]:pr-2 min-[320px]:pt-2 lg:pl-9 lg:pt-4 pb-4 md:pt-4 md:pl-9 sm:pl-5 sm:pt-2 flex flex-col justify-between">
-                    <div>
-                      <h3 class="sm:text-xs md:text-base lg:text-xl font-semibold mb-2 lg:pr-6 md:pr-4 sm:pr-2   min-[320px]:text-sm">
-                        {result.Product_Name}
-                      </h3>
-                      <p class="text-white-600 lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2 sm:text-xs md:text-base lg:text-base  min-[320px]:text-sm">
-                        {result.Price}
-                      </p>
-                    </div>
-                    <div class="mt-2 sm:text-xs md:text-base lg:text-base  min-[320px]:text-xs">
-                      <ul class="list-disc lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2">
-                        <li>{result.Processor}</li>
-                        <li>{result.Memory}</li>
-                      </ul>
-                    </div>
-                    <p className={`font-semibold lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2 sm:text-xs md:text-base lg:text-base  min-[320px]:text-sm ${
+  <img src={result.Image} alt="Laptop Photo" class="w-52  h-auto rounded-lg"/>
+  <div class=" min-[320px]:pl-2 min-[320px]:pr-2 min-[320px]:pt-2 lg:pl-9 lg:pt-4 pb-4 md:pt-4 md:pl-9 sm:pl-5 sm:pt-2 flex flex-col justify-between">
+    <div>
+      <h3 class="sm:text-xs md:text-base lg:text-xl font-semibold mb-2 lg:pr-6 md:pr-4 sm:pr-2   min-[320px]:text-sm">{result.Product_Name}</h3>
+      <p class="text-white-600 lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2 sm:text-xs md:text-base lg:text-base  min-[320px]:text-sm">{result.Price}</p>
+    </div>
+    <p className={`font-semibold lg:pl-2 lg:pr-3 md:pl-2 md:pr-3 sm:pl-1 sm:pr-2 sm:text-xs md:text-base lg:text-base  min-[320px]:text-sm ${
   result.order_status === 1
     ? 'text-yellow-500' // Yellow for 'Packed'
     : result.order_status === 2
@@ -142,9 +128,9 @@ console.log(productData)
     ? 'Delivered'
     : 'Unknown Status'}
 </p>
-
-                  </div>
-                </div>
+<button class="min-[320px]:text-xs sm:text-xs md:text-base lg:text-base min-[320px]:mt-2 min-[320px]:mr-2 lg:mt-4 lg:mr-3 md:mt-4 md:mr-3 sm:mt-2 sm:mr-1  bg-red-600 text-white rounded-md min-[320px]:px-2 min-[320px]:py-1 lg:px-4 lg:py-2 md:px-4 md:py-2 sm:px-2 sm:py-2">Cancel Order</button>
+  </div>
+</div>
               </>
             ))}
       </div>
